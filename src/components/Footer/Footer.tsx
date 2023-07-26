098a2e1bbdae71dd49ac5d13d0a13cac1e7a88fa
Varun Gupta <varungupta2015135@gmail.com>
2023-07-26 13:03:15 +0530

    added buttons to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,16 @@
 export const Footer = () => {
-  return <div className="flex h-[60px] px-28 items-center border-t">test</div>;
+  return (
+    <div className="grid h-[60px] px-28 items-center border-t grid-cols-[1fr,1fr,auto]">
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item">
+        <div className="inline-block rounded px-5 py-1 w-fit text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500">
+          Cancel
+        </div>
+        <div className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-1 w-fit text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 ml-5">
+          Continue
+        </div>
+      </div>
+    </div>
+  );
 };
