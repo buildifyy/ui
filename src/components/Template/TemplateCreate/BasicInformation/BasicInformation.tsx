45bdf10fe039b7ae4c341cd40d3e34bbc4d98351
Varun Gupta <varungupta2015135@gmail.com>
2023-07-27 06:33:02 +0530

    refactored to have a separate component for basic information

---
+++ src/components/Template/TemplateCreate/BasicInformation/BasicInformation.tsx
@@ -0,0 +1,118 @@
+import { Select, Toggle } from "../../..";
+
+interface BasicInformationProps {
+  readonly parent: string;
+  readonly onChangeParent: (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => void;
+  readonly name: string;
+  readonly onChangeName: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  readonly externalId: string;
+  readonly onChangeExternalId: (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => void;
+}
+
+export const BasicInformation = (props: BasicInformationProps) => {
+  const {
+    parent,
+    onChangeParent,
+    name,
+    onChangeName,
+    externalId,
+    onChangeExternalId,
+  } = props;
+
+  return (
+    <div className="flex flex-col mt-5 mx-10 border rounded py-5 items-center overflow-scroll h-[27rem]">
+      <div className="flex items-center w-full justify-around">
+        <div className="flex flex-col w-96">
+          <label
+            htmlFor="parent"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Parent Template
+          </label>
+          <span className="text-xs text-gray-400 mt-2 w-60">
+            Attributes, Relationships and Metric Types will be inherited from
+            this template.
+          </span>
+        </div>
+        <Select
+          id="parent"
+          name="parent"
+          onChange={onChangeParent}
+          value={parent}
+          widthClassName="w-64"
+        />
+      </div>
+      <hr className="w-[84%] my-6" />
+      <div className="flex items-center w-full justify-around">
+        <div className="flex flex-col w-96">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Template Name
+          </label>
+          <span className="text-xs text-gray-400 mt-2 w-60">
+            This will be the name of your template.
+          </span>
+        </div>
+        <input
+          id="name"
+          type="text"
+          className="w-64 border h-[38px] p-2 rounded shadow-sm sm:text-sm text-gray-700"
+          required
+          value={name}
+          onChange={onChangeName}
+        />
+      </div>
+      <hr className="w-[84%] my-6" />
+      <div className="flex items-center w-full justify-around">
+        <div className="flex flex-col w-96">
+          <label
+            htmlFor="externalId"
+            className="block text-sm font-medium text-gray-700"
+          >
+            External ID
+          </label>
+          <span className="text-xs text-gray-400 mt-2">
+            A unique identifier for your template.
+          </span>
+        </div>
+        <input
+          id="externalId"
+          type="text"
+          className="w-64 border h-[38px] p-2 rounded shadow-sm sm:text-sm text-gray-700"
+          required
+          value={externalId}
+          onChange={onChangeExternalId}
+        />
+      </div>
+      <hr className="w-[84%] my-6" />
+      <div className="flex items-center w-full justify-around">
+        <div className="flex flex-col w-96">
+          <label
+            htmlFor="custom"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Custom
+          </label>
+          <span className="text-xs text-gray-400 mt-2">
+            Specifies if the template is custom or out-of-the-box.
+          </span>
+        </div>
+        <div className="flex w-64">
+          <div className="flex flex-col w-96 items-end">
+            <Toggle id="custom" name="custom" value={true} isDisabled />
+            <span className="text-xs text-gray-400 mt-2">
+              This value cannot be changed
+            </span>
+          </div>
+        </div>
+      </div>
+      <hr className="w-[84%] my-6" />
+    </div>
+  );
+};
--- src/components/Template/TemplateCreate/TemplateCreate.tsx
+++ src/components/Template/TemplateCreate/TemplateCreate.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Header, Select, Toggle } from "../..";
+import { Header } from "../..";
+import { BasicInformation } from "./BasicInformation";
 
 interface TemplateCreateProps {
   readonly stepSelection:
@@ -41,96 +42,14 @@ export const TemplateCreate = ({
       <Header value={stepSelection} />
       <form onSubmit={handleSubmit}>
         {stepSelection === "Basic Information" ? (
-          <div className="flex flex-col mt-5 mx-10 border rounded py-5 items-center overflow-scroll h-[27rem]">
-            <div className="flex items-center w-full justify-around">
-              <div className="flex flex-col w-96">
-                <label
-                  htmlFor="parent"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Parent Template
-                </label>
-                <span className="text-xs text-gray-400 mt-2 w-60">
-                  Attributes, Relationships and Metric Types will be inherited
-                  from this template.
-                </span>
-              </div>
-              <Select
-                id="parent"
-                name="parent"
-                onChange={handleParentChange}
-                value={parent}
-                widthClassName="w-64"
-              />
-            </div>
-            <hr className="w-[84%] my-6" />
-            <div className="flex items-center w-full justify-around">
-              <div className="flex flex-col w-96">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Template Name
-                </label>
-                <span className="text-xs text-gray-400 mt-2 w-60">
-                  This will be the name of your template.
-                </span>
-              </div>
-              <input
-                id="name"
-                type="text"
-                className="w-64 border h-[38px] p-2 rounded shadow-sm sm:text-sm text-gray-700"
-                required
-                value={name}
-                onChange={handleNameChange}
-              />
-            </div>
-            <hr className="w-[84%] my-6" />
-            <div className="flex items-center w-full justify-around">
-              <div className="flex flex-col w-96">
-                <label
-                  htmlFor="externalId"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  External ID
-                </label>
-                <span className="text-xs text-gray-400 mt-2">
-                  A unique identifier for your template.
-                </span>
-              </div>
-              <input
-                id="externalId"
-                type="text"
-                className="w-64 border h-[38px] p-2 rounded shadow-sm sm:text-sm text-gray-700"
-                required
-                value={externalId}
-                onChange={handleExternalIdChange}
-              />
-            </div>
-            <hr className="w-[84%] my-6" />
-            <div className="flex items-center w-full justify-around">
-              <div className="flex flex-col w-96">
-                <label
-                  htmlFor="custom"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Custom
-                </label>
-                <span className="text-xs text-gray-400 mt-2">
-                  Specifies if the template is custom or out-of-the-box.
-                </span>
-              </div>
-              <div className="flex w-64">
-                <div className="flex flex-col w-96 items-end">
-                  <Toggle id="custom" name="custom" value={true} isDisabled />
-                  <span className="text-xs text-gray-400 mt-2">
-                    This value cannot be changed
-                  </span>
-                </div>
-              </div>
-            </div>
-            <hr className="w-[84%] my-6" />
-          </div>
+          <BasicInformation
+            parent={parent}
+            onChangeParent={handleParentChange}
+            name={name}
+            onChangeName={handleNameChange}
+            externalId={externalId}
+            onChangeExternalId={handleExternalIdChange}
+          />
         ) : null}
       </form>
     </div>
