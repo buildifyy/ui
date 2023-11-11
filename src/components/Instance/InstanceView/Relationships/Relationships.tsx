84f932c467992ba293a219e4bc975a94a9510d69
Varun Gupta <varungupta2015135@gmail.com>
2023-11-11 21:55:38 +0530

    moved relationship graph to be such that all relationships are shown in one graph

--- src/components/Instance/InstanceView/Relationships/Relationships.tsx
+++ src/components/Instance/InstanceView/Relationships/Relationships.tsx
@@ -1,7 +1,12 @@
 import { FormLabel } from "@/components/ui/form";
 import { InstanceFormData } from "@/models";
 import { useRelationships } from "@/service";
-import Graphin, { Behaviors, GraphinData } from "@antv/graphin";
+import Graphin, {
+  Behaviors,
+  GraphinData,
+  IUserEdge,
+  IUserNode,
+} from "@antv/graphin";
 import {
   Chip,
   Popover,
@@ -10,11 +15,7 @@ import {
 } from "@nextui-org/react";
 import { Eye } from "lucide-react";
 import { useState } from "react";
-import {
-  FieldArrayWithId,
-  useFieldArray,
-  useFormContext,
-} from "react-hook-form";
+import { useFieldArray, useFormContext } from "react-hook-form";
 
 export const Relationships = () => {
   const { control, getValues } = useFormContext<InstanceFormData>();
@@ -31,43 +32,62 @@ export const Relationships = () => {
 
   const { data: relationshipTemplates } = useRelationships();
 
-  const handlePopoverClick = (
-    relationship: FieldArrayWithId<InstanceFormData, "relationships", "_id">,
-    isOpen: boolean
-  ) => {
+  const handlePopoverClick = (isOpen: boolean) => {
     if (isOpen) {
       const source = getValues("basicInformation.name");
-      const targets = Array.isArray(relationship.target)
-        ? relationship.target.map((target) => target)
-        : [relationship.target];
-      const relationshipName = relationshipTemplates?.find(
-        (template) => template.id === relationship.relationshipTemplateId
-      )?.name;
+      const nodes: IUserNode[] = [
+        {
+          id: source,
+          style: {
+            label: {
+              value: source,
+            },
+          },
+        },
+      ];
 
-      const allNodes = [source, ...targets];
+      const edges: IUserEdge[] = [];
+
+      relationships.forEach((relationship) => {
+        const relationshipName = relationshipTemplates?.find(
+          (template) => template.id === relationship.relationshipTemplateId
+        )?.name;
+
+        const targets = Array.isArray(relationship.target)
+          ? relationship.target.map((target) => target)
+          : [relationship.target ?? ""];
+
+        nodes.push(
+          ...targets.map((target) => {
+            return {
+              id: target,
+              style: {
+                label: {
+                  value: target,
+                },
+              },
+            };
+          })
+        );
+
+        edges.push(
+          ...targets.map((target) => {
+            return {
+              source: source,
+              target: target,
+              style: {
+                label: {
+                  value: relationshipName,
+                },
+              },
+            };
+          })
+        );
+      });
 
       setGraphDataToRender({
-        nodes: allNodes.map((node) => {
-          return {
-            id: node ?? "",
-            style: {
-              label: {
-                value: node ?? "",
-              },
-            },
-          };
-        }),
-        edges: targets.map((target) => {
-          return {
-            source: source,
-            target: target ?? "",
-            style: {
-              label: {
-                value: relationshipName,
-              },
-            },
-          };
-        }),
+        nodes,
+        edges,
       });
     }
   };
@@ -77,48 +97,46 @@ export const Relationships = () => {
   return (
     <div className="flex flex-col mt-5 mx-10 border rounded py-10 px-10 items-center overflow-y-auto h-[calc(100vh-220px)] lg:mx-[20%] md:mx-[15%] sm:mx-[5%] xs:mx-0">
       <div className="space-y-4 w-full">
-        {getValues("basicInformation.name")}
+        <div className="flex gap-3 items-center">
+          <span>{getValues("basicInformation.name")}</span>
+          <Popover
+            showArrow
+            placement="right"
+            onOpenChange={handlePopoverClick}
+            backdrop="blur"
+          >
+            <PopoverTrigger>
+              <Eye width={17} height={17} className="hover:cursor-pointer" />
+            </PopoverTrigger>
+            <PopoverContent className="p-0">
+              <Graphin
+                data={graphDataToRender}
+                theme={{
+                  mode: "dark",
+                  background:
+                    "hsl(var(--nextui-content1) / var(--nextui-content1-opacity, var(--tw-bg-opacity)))",
+                  edgeSize: 2,
+                  nodeSize: 30,
+                }}
+                style={{
+                  borderRadius: "10px",
+                }}
+              >
+                <ZoomCanvas />
+              </Graphin>
+            </PopoverContent>
+          </Popover>
+        </div>
         {relationships.map((relationship) => {
           return (
             <div className="flex flex-col m-4" key={relationship.id}>
-              <div className="flex gap-3">
-                <FormLabel className="block font-medium">
-                  {
-                    relationshipTemplates?.find(
-                      (r) => r.id === relationship.relationshipTemplateId
-                    )?.name
-                  }
-                </FormLabel>
-                <Popover
-                  showArrow
-                  placement="right"
-                  onOpenChange={(isOpen: boolean) =>
-                    handlePopoverClick(relationship, isOpen)
-                  }
-                  backdrop="blur"
-                >
-                  <PopoverTrigger>
-                    <Eye width={17} height={17} />
-                  </PopoverTrigger>
-                  <PopoverContent className="p-0">
-                    <Graphin
-                      data={graphDataToRender}
-                      theme={{
-                        mode: "dark",
-                        background:
-                          "hsl(var(--nextui-content1) / var(--nextui-content1-opacity, var(--tw-bg-opacity)))",
-                        edgeSize: 2,
-                        nodeSize: 30,
-                      }}
-                      style={{
-                        borderRadius: "10px",
-                      }}
-                    >
-                      <ZoomCanvas />
-                    </Graphin>
-                  </PopoverContent>
-                </Popover>
-              </div>
+              <FormLabel className="block font-medium">
+                {
+                  relationshipTemplates?.find(
+                    (r) => r.id === relationship.relationshipTemplateId
+                  )?.name
+                }
+              </FormLabel>
 
               <div className="flex flex-wrap gap-2 mt-3">
                 {Array.isArray(relationship.target) ? (
