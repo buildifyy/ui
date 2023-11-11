d69b69f3875d726882bc3131707ca8bd980fc8d7
Varun Gupta <varungupta2015135@gmail.com>
2023-11-11 20:46:33 +0530

    added initial graph view in instance relationships

--- src/components/Instance/InstanceView/Relationships/Relationships.tsx
+++ src/components/Instance/InstanceView/Relationships/Relationships.tsx
@@ -1,11 +1,27 @@
 import { FormLabel } from "@/components/ui/form";
 import { InstanceFormData } from "@/models";
 import { useRelationships } from "@/service";
-import { Chip } from "@nextui-org/react";
-import { useFieldArray, useFormContext } from "react-hook-form";
+import Graphin, { Behaviors, GraphinData } from "@antv/graphin";
+import {
+  Chip,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { Eye } from "lucide-react";
+import { useState } from "react";
+import {
+  FieldArrayWithId,
+  useFieldArray,
+  useFormContext,
+} from "react-hook-form";
 
 export const Relationships = () => {
   const { control, getValues } = useFormContext<InstanceFormData>();
+  const [graphDataToRender, setGraphDataToRender] = useState<GraphinData>({
+    nodes: [],
+    edges: [],
+  });
 
   const { fields: relationships } = useFieldArray({
     control,
@@ -15,6 +31,94 @@ export const Relationships = () => {
 
   const { data: relationshipTemplates } = useRelationships();
 
+  //   const graphDataToRender: GraphinData = {
+  //     nodes: [
+  //       {
+  //         id: "node-0",
+  //         x: 100,
+  //         y: 100,
+  //         style: {
+  //           label: {
+  //             value: "node-0",
+  //           },
+  //         },
+  //       },
+  //       {
+  //         id: "node-1",
+  //         x: 200,
+  //         y: 200,
+  //         style: {
+  //           label: {
+  //             value: "node-1",
+  //           },
+  //         },
+  //       },
+  //       {
+  //         id: "node-2",
+  //         x: 100,
+  //         y: 300,
+  //         style: {
+  //           label: {
+  //             value: "node-2",
+  //           },
+  //         },
+  //       },
+  //     ],
+  //     edges: [
+  //       {
+  //         source: "node-0",
+  //         target: "node-1",
+  //       },
+  //       {
+  //         source: "node-0",
+  //         target: "node-2",
+  //       },
+  //     ],
+  //   };
+
+  const handlePopoverClick = (
+    relationship: FieldArrayWithId<InstanceFormData, "relationships", "_id">,
+    isOpen: boolean
+  ) => {
+    if (isOpen) {
+      const source = getValues("basicInformation.name");
+      const targets = Array.isArray(relationship.target)
+        ? relationship.target.map((target) => target)
+        : [relationship.target];
+      const relationshipName = relationshipTemplates?.find(
+        (template) => template.id === relationship.relationshipTemplateId
+      )?.name;
+
+      const allNodes = [source, ...targets];
+
+      setGraphDataToRender({
+        nodes: allNodes.map((node) => {
+          return {
+            id: node ?? "",
+            style: {
+              label: {
+                value: node ?? "",
+              },
+            },
+          };
+        }),
+        edges: targets.map((target) => {
+          return {
+            source: source,
+            target: target ?? "",
+            style: {
+              label: {
+                value: relationshipName,
+              },
+            },
+          };
+        }),
+      });
+    }
+  };
+
+  const { ZoomCanvas } = Behaviors;
+
   return (
     <div className="flex flex-col mt-5 mx-10 border rounded py-10 px-10 items-center overflow-y-auto h-[calc(100vh-220px)] lg:mx-[20%] md:mx-[15%] sm:mx-[5%] xs:mx-0">
       <div className="space-y-4 w-full">
@@ -22,13 +126,39 @@ export const Relationships = () => {
         {relationships.map((relationship) => {
           return (
             <div className="flex flex-col m-4" key={relationship.id}>
-              <FormLabel className="block font-medium">
-                {
-                  relationshipTemplates?.find(
-                    (r) => r.id === relationship.relationshipTemplateId
-                  )?.name
-                }
-              </FormLabel>
+              <div className="flex gap-3">
+                <FormLabel className="block font-medium">
+                  {
+                    relationshipTemplates?.find(
+                      (r) => r.id === relationship.relationshipTemplateId
+                    )?.name
+                  }
+                </FormLabel>
+                <Popover
+                  showArrow
+                  placement="right"
+                  onOpenChange={(isOpen: boolean) =>
+                    handlePopoverClick(relationship, isOpen)
+                  }
+                >
+                  <PopoverTrigger>
+                    <Eye width={17} height={17} />
+                  </PopoverTrigger>
+                  <PopoverContent className="p-0">
+                    <Graphin
+                      data={graphDataToRender}
+                      //   layout={{ type: "radial" }}
+                      theme={{ mode: "dark" }}
+                      style={{
+                        backgroundColor: "transparent",
+                        borderRadius: "10px",
+                      }}
+                    >
+                      <ZoomCanvas />
+                    </Graphin>
+                  </PopoverContent>
+                </Popover>
+              </div>
 
               <div className="flex flex-wrap gap-2 mt-3">
                 {Array.isArray(relationship.target) ? (
